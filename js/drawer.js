8aa4ba4928b26a2bfe913f99db84b200f1d60b67
Brandyn Burbank <bur11012@byui.edu>
2014-01-16 16:56:53 -0700

    continued work on mobile sidebar

--- js/drawer.js
+++ js/drawer.js
@@ -5,8 +5,20 @@ $(document).ready(function() {
 	
 })
 
+var drawerOpen = false;
+
 function open_side()
 {
-	$("#sidebar").css("display","block");
-	$("body").css("margin-left","-100px");
+	if (!drawerOpen)
+	{
+		$("#sidebar").css("display","block");
+		$("body").css("left","-250px");
+		drawerOpen = true;
+	}
+	else
+	{
+		$("#sidebar").css("display","none");
+		$("body").css("left","0px");
+		drawerOpen = false;
+	}
 }
