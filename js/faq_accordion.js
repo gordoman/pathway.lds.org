7ed5913f7297ed266127b1cd650557cdd70ea896
Brandyn Burbank <bur11012@byui.edu>
2014-01-22 17:04:33 -0700

    worked on faq accordion

--- js/faq_accordion.js
+++ js/faq_accordion.js
@@ -1,11 +1,19 @@
+$(document).ready(function() {
+	//var height = $('p.collapsible').height();
+	$('p.collapsible').each(function (){
+		$(this).attr('xheight', $(this).height());
+		$(this).css('height','0px');
+	});
+});
+
 function expand(object)
 {
 	if (($("#"+object).height()) == 0)
 	{
-		$("#"+object).animate({ "height": "100" }, "slow" );
+		$("#"+object).animate({ "height": ($("#"+object).attr('xheight')+'px') }, "slow" );
 	}
 	else
 	{
-		$("#"+object).animate({ "height": "0" }, "slow" );
+		$("#"+object).animate({ "height": "0px" }, "slow" );
 	}
 }
