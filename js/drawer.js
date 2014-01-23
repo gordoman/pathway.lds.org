7de9668d75e9205b35abfaea77cbe0f961b600b6
Brandyn Burbank <bur11012@byui.edu>
2014-01-23 16:59:47 -0700

    cleaned up deprecated files

--- js/drawer.js
+++ js/drawer.js
@@ -30,7 +30,7 @@ function open_side()
 $(document).ready(function() {
 	var main_height = $("#main-container").height();
 	var sidebar_height = $(".pf-sidebar").height();
-	var diffheight = main_height-sidebar_height
+	//var diffheight = main_height-sidebar_height
 	//$("#sidebar").css("padding-bottom", diffheight);
 	$("#sidebar").css("height", main_height);
 });
